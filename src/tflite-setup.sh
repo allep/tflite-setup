fd7d3fcaa9caf82cb8b29de45826f68cbb5e6e6a
Alessandro Paganelli <alessandro.paganelli@zuru.com>
2024-01-23 16:46:06 +0100

    Added bazel support

--- src/tflite-setup.sh
+++ src/tflite-setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 TFLITE_REPO_URL="git@github.com:tensorflow/tflite-micro.git"
+BAZEL_BIN="bazelisk-linux-amd64" 
+BAZEL_URL="https://github.com/bazelbuild/bazelisk/releases/download/v1.19.0/${BAZEL_BIN}"
+USER_HOME_BIN="${HOME}/bin"
 
 # Internal state
 
@@ -8,9 +11,18 @@ INSTALL_TARGET_DIR=""
 INSTALL_MODE="false"
 
 # Functions
+
 tflite_repo_setup() {
-    if [[ "$INSTALL_MODE" == "true" ]]; then
-        git clone "$TFLITE_REPO_URL" "$INSTALL_TARGET_DIR"
+    if [[ "${INSTALL_MODE}" == "true" ]]; then
+        git clone "${TFLITE_REPO_URL}" "${INSTALL_TARGET_DIR}"
+    fi
+}
+
+bazel_setup() {
+    if [[ "${INSTALL_MODE}" == "true" ]]; then
+        local target_bin="${USER_HOME_BIN}/${BAZEL_BIN}"
+        wget -o "${target_bin}" "${BAZEL_URL}" 
+        chmod +x "${target_bin}"
     fi
 }
 
@@ -29,6 +41,7 @@ print_script_status() {
     echo "Running script with:"
     echo "INSTALL_MODE:             ${INSTALL_MODE}"
     echo "INSTALL_TARGET_DIR:       ${INSTALL_TARGET_DIR}"
+    echo "Bazel bin path:           ${USER_HOME_BIN}/${BAZEL_BIN}"
 }
 
 # Actual script
@@ -36,4 +49,5 @@ check_script_params $@
 print_script_status
 
 tflite_repo_setup
+bazel_setup
 
